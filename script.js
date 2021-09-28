b58ed607a82e67dcf000247ad30fb7f7678a9083
almondjain <49396182+almondjain@users.noreply.github.com>
2021-09-28 14:13:04 +0530

    beautify

--- script.js
+++ script.js
@@ -4,32 +4,30 @@ var currentPrice = document.querySelector("#current-price");
 var checkBtn = document.querySelector("#check-btn");
 var output = document.querySelector(".output");
 
-function calculateProfitLoss(cp,qty,sp){
-    if(cp>sp){
+function calculateProfitLoss(cp, qty, sp) {
+    if (cp > sp) {
         //loss
-        var loss = (cp-sp)*qty;
-        var lossPer = (loss/cp)*100;
+        var loss = (cp - sp) * qty;
+        var lossPer = (loss / cp) * 100;
 
         output.innerText = `Ohho, You are in Loss! The Loss is ${loss} and the percent is ${lossPer}%`;
-    }
-    else if(sp>cp){
+    } else if (sp > cp) {
         //profit
-        var profit = (sp-cp)*qty;
-        var profitPer = (profit/cp)*100;
+        var profit = (sp - cp) * qty;
+        var profitPer = (profit / cp) * 100;
 
         output.innerText = `Yayaya, You got the Profit! The Profit is ${profit} and the percent is ${profitPer}%`;
-    }
-    else{
+    } else {
         output.innerText = `Take a sigh! No Profit, No Loss`;
     }
 }
 
-function profitLoss(){
+function profitLoss() {
     var costPrice = Number(initialPrice.value);
     var quantity = Number(stocksQuantity.value);
     var sellingPrice = Number(currentPrice.value);
 
-    calculateProfitLoss(costPrice,quantity,sellingPrice);
+    calculateProfitLoss(costPrice, quantity, sellingPrice);
 }
 
-checkBtn.addEventListener("click",profitLoss);
+checkBtn.addEventListener("click", profitLoss);
